95006edc7f4bdc4dadff926924d7b953272a1035
sabyasachiNITD <sabyasachi9733@gmail.com>
2024-09-22 13:45:11 +0530

    Frequency Of Elements in List ---added

--- src/main/java/com/villagecoder/frequencyOfNumbers/Main.java
+++ src/main/java/com/villagecoder/frequencyOfNumbers/Main.java
@@ -8,10 +8,12 @@ package com.villagecoder.frequencyOfNumbers;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(2,5,2,8,7,1,6,6,7,9,11,1,4);
-
+        list.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+                .entrySet().stream().forEach(entry -> System.out.println(entry.getKey() + "-> " + entry.getValue()));
     }
 }
